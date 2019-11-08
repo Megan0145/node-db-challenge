a7882e1ec709a60c2992c5c5cb8b678b7fe8894a
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-08 09:59:22 +0000

    Add database config file- invoke knex

---
+++ projects/projects-model.js
@@ -0,0 +1,6 @@
+//insert into resources(resource_name, description) values("Lecturer", "Great teacher")
+//select * from resources
+//insert into projects(project_name, description, completed) values("Finish sprint", "Polish everything up", 0)
+//select * from projects
+//insert into tasks(project_id, description, completed) values(1, "Go back over past projects", 0)
+//select * from tasks
